1c70c678fbf57fb2820318b891c5216220650eb9
Leonardo Alegretti <64331203+alegrettiLeonardo@users.noreply.github.com>
2026-09-08 20:30:23 -0300

    feat: add shared UI widget facade

---
+++ src/ross_studio/widgets.py
@@ -0,0 +1,11 @@
+from .charts import BearingCoefficientChart, CampbellChart, RotorSketch
+from .ui_shell import (
+    AppToolbar, Card, LabelValueGrid, ModelSummaryCard, ProjectInfoCard,
+    QuickActionsCard, SectionCard, Sidebar, StatusBar, configure_table, item,
+)
+
+__all__ = [
+    "AppToolbar", "BearingCoefficientChart", "CampbellChart", "Card",
+    "LabelValueGrid", "ModelSummaryCard", "ProjectInfoCard", "QuickActionsCard",
+    "RotorSketch", "SectionCard", "Sidebar", "StatusBar", "configure_table", "item",
+]
