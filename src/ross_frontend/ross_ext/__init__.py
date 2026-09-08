98503401de9babb6413c7e6e4f978710bdb7b5c6
Leonardo Alegretti <64331203+alegrettiLeonardo@users.noreply.github.com>
2026-09-08 17:04:01 -0300

    chore(ump): export transient RHS integration helpers

--- src/ross_frontend/ross_ext/__init__.py
+++ src/ross_frontend/ross_ext/__init__.py
@@ -2,16 +2,20 @@
 
 from .ump import (
     LEGACY_KGF_MM2_TO_N_M2,
+    UMPElementContribution,
     consistent_ump_matrix,
     legacy_ump_to_si,
+    make_ump_rhs_callback,
     make_ump_shaft_element_class,
     ump_force_from_displacement,
 )
 
 __all__ = [
     "LEGACY_KGF_MM2_TO_N_M2",
+    "UMPElementContribution",
     "consistent_ump_matrix",
     "legacy_ump_to_si",
+    "make_ump_rhs_callback",
     "make_ump_shaft_element_class",
     "ump_force_from_displacement",
 ]
