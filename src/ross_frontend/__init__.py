7e2832ff195da127c86e54d1af3072365f95c2ae
Leonardo Alegretti <64331203+alegrettiLeonardo@users.noreply.github.com>
2026-09-08 15:10:56 -0300

    feat: initialize ROSS frontend domain package

---
+++ src/ross_frontend/__init__.py
@@ -0,0 +1,33 @@
+"""ROSS Frontend integration core."""
+
+__version__ = "0.1.0"
+
+from .domain import (
+    AnalysisRequest,
+    BallBearingSpec,
+    CoefficientBearingSpec,
+    CylindricalBearingSpec,
+    DiskSpec,
+    MaterialSpec,
+    PlainJournalBearingSpec,
+    PointMassSpec,
+    RollerBearingSpec,
+    RotorProject,
+    ShaftSectionSpec,
+    TiltingPadBearingSpec,
+)
+
+__all__ = [
+    "AnalysisRequest",
+    "BallBearingSpec",
+    "CoefficientBearingSpec",
+    "CylindricalBearingSpec",
+    "DiskSpec",
+    "MaterialSpec",
+    "PlainJournalBearingSpec",
+    "PointMassSpec",
+    "RollerBearingSpec",
+    "RotorProject",
+    "ShaftSectionSpec",
+    "TiltingPadBearingSpec",
+]
