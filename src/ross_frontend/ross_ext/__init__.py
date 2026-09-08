8cd431d6c1603913ec5c65088a81423e2138f598
Leonardo Alegretti <64331203+alegrettiLeonardo@users.noreply.github.com>
2026-09-08 18:40:18 -0300

    refactor(ump): expose only global stiffness extension APIs

--- src/ross_frontend/ross_ext/__init__.py
+++ src/ross_frontend/ross_ext/__init__.py
@@ -2,20 +2,28 @@
 
 from .ump import (
     LEGACY_KGF_MM2_TO_N_M2,
+    ROTORDIN_LEGACY_MAX_REGIONS,
+    ROTORDIN_LEGACY_THRESHOLD_N_M2,
+    UMPAssembly,
     UMPElementContribution,
+    UMPElementSpan,
+    assemble_ump_global,
     consistent_ump_matrix,
     legacy_ump_to_si,
-    make_ump_rhs_callback,
-    make_ump_shaft_element_class,
-    ump_force_from_displacement,
+    rotordin_legacy_ump_matrix,
 )
+from .ump_rotor import make_ump_rotor_class
 
 __all__ = [
     "LEGACY_KGF_MM2_TO_N_M2",
+    "ROTORDIN_LEGACY_MAX_REGIONS",
+    "ROTORDIN_LEGACY_THRESHOLD_N_M2",
+    "UMPAssembly",
     "UMPElementContribution",
+    "UMPElementSpan",
+    "assemble_ump_global",
     "consistent_ump_matrix",
     "legacy_ump_to_si",
-    "make_ump_rhs_callback",
-    "make_ump_shaft_element_class",
-    "ump_force_from_displacement",
+    "make_ump_rotor_class",
+    "rotordin_legacy_ump_matrix",
 ]
