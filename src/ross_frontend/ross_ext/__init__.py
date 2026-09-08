5762dfd98599682982937960269891098fdfa627
Leonardo Alegretti <64331203+alegrettiLeonardo@users.noreply.github.com>
2026-09-08 16:56:01 -0300

    feat(ump): create ROSS scientific extension package

---
+++ src/ross_frontend/ross_ext/__init__.py
@@ -0,0 +1,17 @@
+"""Scientific extensions that preserve RotorDin-specific physics on top of ROSS."""
+
+from .ump import (
+    LEGACY_KGF_MM2_TO_N_M2,
+    consistent_ump_matrix,
+    legacy_ump_to_si,
+    make_ump_shaft_element_class,
+    ump_force_from_displacement,
+)
+
+__all__ = [
+    "LEGACY_KGF_MM2_TO_N_M2",
+    "consistent_ump_matrix",
+    "legacy_ump_to_si",
+    "make_ump_shaft_element_class",
+    "ump_force_from_displacement",
+]
